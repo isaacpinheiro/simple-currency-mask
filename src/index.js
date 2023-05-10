e89990148f66047d094c3f5614bc4cf4f949e874
Isaac Pinheiro <isaacpnhr@gmail.com>
2023-05-09 23:01:03 -0300

    Writing tests for mask and unmask functions.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 "use strict";
+
+function mask(strVal, config = null) {
+  return "";
+}
+
+function unmask(strVal, config = null) {
+  return "";
+}
+
+function dynamicMask(strVal, config = null) {
+  return "";
+}
+
+module.exports = {
+  mask: mask,
+  unmask: unmask,
+  dynamicMask: dynamicMask
+};
--- test/mask.test.js
+++ test/mask.test.js
@@ -1,5 +1,93 @@
 "use strict";
 
-test("Just a test.", () => {
-  expect(true).toBe(true);
+const { mask, unmask, dynamicMask } = require("../src/index");
+
+test("01 - Testing mask function without the config object.", () => {
+  expect(mask("1234567.89")).toBe("1,234,567.89");
+  expect(mask("-1234567.89")).toBe("-1,234,567.89");
 });
+
+test("02 - Testing unmask function without the config object.", () => {
+  expect(unmask("1,234,567.89")).toBe("1234567.89");
+  expect(unmask("-1,234,567.89")).toBe("-1234567.89");
+});
+
+test("03 - Testing dynamicMask function without the config object.", () => {
+  expect(dynamicMask("1234567.89")).toBe("1,234,567.89");
+  expect(dynamicMask("-1234567.89")).toBe("-1,234,567.89");
+});
+
+test("04 - Testing mask function with the config object.", () => {
+
+  let config = { decimalSeparator: ",", currency: "R$" };
+
+  expect(mask("1234567.89", config)).toBe("R$ 1.234.567,89");
+  expect(mask("-1234567.89", config)).toBe("R$ -1.234.567,89");
+
+});
+
+test("05 - Testing mask function without the currency attribute.", () => {
+
+  let config = { decimalSeparator: "," };
+
+  expect(mask("1234567.89", config)).toBe("1.234.567,89");
+  expect(mask("-1234567.89", config)).toBe("-1.234.567,89");
+
+});
+
+test("06 - Testing mask function without the decimalSeparator attribute.", () => {
+
+  let config = { currency: "R$" };
+
+  expect(mask("1234567.89", config)).toBe("R$ 1,234,567.89");
+  expect(mask("-1234567.89", config)).toBe("R$ -1,234,567.89");
+
+});
+
+test("07 - Testing mask function without the decimalSeparator = '.'.", () => {
+
+  let config = { decimalSeparator: "." };
+
+  expect(mask("1234567.89", config)).toBe("1,234,567.89");
+  expect(mask("-1234567.89", config)).toBe("-1,234,567.89");
+
+});
+
+test("08 - Testing unmask function with the config object.", () => {
+
+  let config = { decimalSeparator: ",", currency: "R$" };
+
+  expect(unmask("R$ 1.234.567,89", config)).toBe("1234567.89");
+  expect(unmask("R$ -1.234.567,89", config)).toBe("-1234567.89");
+
+});
+
+test("09 - Testing unmask function without the currency attribute.", () => {
+
+  let config = { decimalSeparator: "," };
+
+  expect(unmask("1.234.567,89", config)).toBe("1234567.89");
+  expect(unmask("-1.234.567,89", config)).toBe("-1234567.89");
+
+});
+
+test("10 - Testing unmask function without the decimalSeparator attribute.", () => {
+
+  let config = { currency: "R$" };
+
+  expect(unmask("R$ 1,234,567.89", config)).toBe("1234567.89");
+  expect(unmask("R$ -1,234,567.89", config)).toBe("-1234567.89");
+
+});
+
+test("11 - Testing unmask function with the decimalSeparator = '.'.", () => {
+
+  let config = { decimalSeparator: "." };
+
+  expect(unmask("1,234,567.89", config)).toBe("1234567.89");
+  expect(unmask("-1,234,567.89", config)).toBe("-1234567.89");
+
+});
+
+// dynamicMask tests
+// TODO
