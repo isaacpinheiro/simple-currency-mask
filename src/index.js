0a9889e4d5b9d9589f5a6f5c3c4a2c8a48dde479
Isaac Pinheiro <isaacpnhr@gmail.com>
2023-05-10 16:46:28 -0300

    Implementing the function 'mask'.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,49 @@
 "use strict";
 
 function mask(strVal, config = null) {
-  return "";
+
+  let res = ""; 
+  let vec = strVal.split(".");
+  let c = 0;
+  let decimalSeparator = ".";
+  let separator = ",";
+  let currency = "";
+
+  if (config !== null) {
+
+    if (config.decimalSeparator !== undefined && config.decimalSeparator !== null
+      && config.decimalSeparator === ",") {
+
+      decimalSeparator = config.decimalSeparator;
+      separator = ".";
+
+    }
+
+    if (config.currency !== undefined && config.currency !== null) {
+      currency = config.currency;
+    }
+
+  }
+
+  for (let i = vec[0].length - 1; i >= 0; i--) {
+
+    if (c !== 0 && c % 3 === 0 && vec[0][i] !== "-") {
+      res = vec[0][i] + separator + res;
+    } else {
+      res = vec[0][i] + res;
+    }   
+
+    c++;
+
+  }
+
+  if (vec[1] !== undefined) res = res + decimalSeparator + vec[1];
+  else res = res + decimalSeparator + "00";
+
+  if (currency !== "") res = currency + " " + res;
+
+  return res;
+
 }
 
 function unmask(strVal, config = null) {
